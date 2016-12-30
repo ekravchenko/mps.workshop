16ada27404e8ab27b777310f1e6a0ec9edac7b41
ekravchenko <yevgen.kravchenko@gmail.com>
2016-12-30 11:54:39 +0200

    #9 Implemented service discovery

--- services/doctor/src/main/java/coza/workshop/doctor/DoctorApplication.java
+++ services/doctor/src/main/java/coza/workshop/doctor/DoctorApplication.java
@@ -3,9 +3,11 @@ package coza.workshop.doctor;
 import coza.workshop.doctor.config.DoctorConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Import;
 
 @Import(DoctorConfig.class)
+@EnableEurekaClient
 @SpringBootApplication
 public class DoctorApplication {
 
