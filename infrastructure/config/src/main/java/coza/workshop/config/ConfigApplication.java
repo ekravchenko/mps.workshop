7cc796918f08f606f4d2f5e76bb9f86dffbded0a
ekravchenko <yevgen.kravchenko@gmail.com>
2016-12-30 11:20:32 +0200

    #3 Introduced centralized config

--- infrastructure/config/src/main/java/coza/workshop/config/ConfigApplication.java
+++ infrastructure/config/src/main/java/coza/workshop/config/ConfigApplication.java
@@ -2,7 +2,9 @@ package coza.workshop.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class ConfigApplication {
     public static void main(String[] args) {
